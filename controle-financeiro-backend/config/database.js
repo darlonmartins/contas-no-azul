8e1468169f1fa7cc3a3ff7e7c110736cdfa59533
Darlon Martins <darlon.martins@hotmail.com>
2025-05-12 17:11:40 -0300

    Ajuste db

--- controle-financeiro-backend/config/database.js
+++ controle-financeiro-backend/config/database.js
@@ -4,9 +4,10 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'controle_financeiro',
   process.env.DB_USER || 'root',
-  process.env.DB_PASS || '147852',
+  process.env.DB_PASS || '',
   {
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
     logging: false,
   }
