11ddfe386039fe01effc052974d64ab22290de88
Darlon Martins <darlon.martins@hotmail.com>
2025-05-13 00:14:12 -0300

    fix: adiciona dependência date-fns

--- controle-financeiro-frontend/vite.config.js
+++ controle-financeiro-frontend/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['lucide-react', 'framer-motion','recharts'], // ✅ força os dois
+      external: ['lucide-react', 'framer-motion','recharts','dayjs'], // ✅ força os dois
     },
   },
 });
