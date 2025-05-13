9df99384bb97e9ee55bebff5b3e8a5c49bfb64ac
Darlon Martins <darlon.martins@hotmail.com>
2025-05-13 19:53:38 -0300

    fix: estrutura final tailwind com postcss funcionando

--- controle-financeiro-frontend/postcss.config.cjs
+++ controle-financeiro-frontend/postcss.config.cjs
@@ -1,6 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
+  plugins: [],
 };
